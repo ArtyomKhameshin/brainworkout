058a5fd7dd5807e1e119358fc85f790014ec575d
Artyom Khameshin <rubbishrabbitsband@gmail.com>
2023-03-04 15:11:21 +0300

    switch to start added to repres. class, ready to start correctly

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,12 @@
 import java.io.File;
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
-    File hello = new File("hello.txt");
-    RepresentFile.representTextFromFile(hello);
 
-        Scanner cmdToStart = new Scanner(System.in);
-        String key = cmdToStart.next();
-        switch (key) {
-            case "h":
-                System.out.println();
-                File help = new File("instructions.txt");
-                RepresentFile.representTextFromFile(help);
-            case "c":
-                System.out.println();
-                System.out.println("Ok, make your calculations below:");
-                System.out.println();
-            default:
-                System.out.println("unknown command");
-        }
-        // обработка ввода для вывода подсказки или перехода к вычислениям
+        File hello = new File("hello.txt");
+        Represent.representTextFromFile(hello); //show hello message
+
+        Represent.representStart(); // switch: to see help or to continue
 
 
 
---
+++ src/Represent.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Represent {
+    // some methods to make the representations in Main easier
+
+    public static void representTextFromFile(File file) {
+        try {
+            Scanner showFile = new Scanner(file);
+            while(showFile.hasNextLine()) {
+                System.out.println(showFile.nextLine());
+            }
+            showFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Lost file to read from, add it to the directory and run app again...");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void representStart () {
+        Scanner cmdToStart = new Scanner(System.in);
+        String key = cmdToStart.next();
+        switch (key) {
+            case "h" -> {
+                File help = new File("instructions.txt");
+                Represent.representTextFromFile(help);
+            }
+            case "c" -> {
+                System.out.println("Ok, make your calculations below:");
+                System.out.println();
+            }
+            default -> {
+                System.out.println("unknown command");
+                representStart();
+            }
+        }
+    }
+
+
+}
