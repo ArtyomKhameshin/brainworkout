5457997a554d57d02535dcf9faa4c68d0e44da61
Artyom Khameshin <rubbishrabbitsband@gmail.com>
2023-03-04 14:35:09 +0300

    Created file with instructions, added switch after hello *no idea how to realize a loop with the default case yet

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
     File hello = new File("hello.txt");
     RepresentFile.representTextFromFile(hello);
 
-       /* Scanner cmdToStart = new Scanner(System.in);
+        Scanner cmdToStart = new Scanner(System.in);
         String key = cmdToStart.next();
         switch (key) {
             case "h":
                 System.out.println();
-
-
-        }*/
+                File help = new File("instructions.txt");
+                RepresentFile.representTextFromFile(help);
+            case "c":
+                System.out.println();
+                System.out.println("Ok, make your calculations below:");
+                System.out.println();
+            default:
+                System.out.println("unknown command");
+        }
         // обработка ввода для вывода подсказки или перехода к вычислениям
 
 
