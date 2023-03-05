ec620893b7027edcdd748fb874cac16007e351f0
Artyom Khameshin <rubbishrabbitsband@gmail.com>
2023-03-05 12:45:15 +0300

    Modified representation of the running
    changes in Represent class
    *added some frame to make the view better
    *added yes/no switch as a method
    changes in Main class
    *the interface in Main is ready to add a calculations mechanize
    *program is ok to run and to show instructions to the user
    *the file with instructions is empty now!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import java.io.File;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
         File hello = new File("hello.txt");
         Represent.representTextFromFile(hello); //show hello message
-
+        Represent.frame();
         Represent.representStart(); // switch: to see help or to continue
+
+
+
 
 
 
--- src/Represent.java
+++ src/Represent.java
@@ -14,21 +14,27 @@ public class Represent {
             showFile.close();
         } catch (FileNotFoundException e) {
             System.out.println("Lost file to read from, add it to the directory and run app again...");
+            frame();
             throw new RuntimeException(e);
         }
     }
 
-    public static void representStart () {
+    public static void representStart() {
         Scanner cmdToStart = new Scanner(System.in);
         String key = cmdToStart.next();
         switch (key) {
             case "h" -> {
                 File help = new File("instructions.txt");
                 Represent.representTextFromFile(help);
+                frame();
+                toDoOrNot();
+                System.out.println("Ok, make your calculations below:");
+                frame();
+
             }
             case "c" -> {
                 System.out.println("Ok, make your calculations below:");
-                System.out.println();
+                frame();
             }
             default -> {
                 System.out.println("unknown command");
@@ -37,6 +43,26 @@ public class Represent {
         }
         cmdToStart.close();
     }
+
+    public static void toDoOrNot () {
+        System.out.println("Do you want to continue?");
+        Scanner yN = new Scanner(System.in);
+        String answer = yN.next();
+        switch (answer) {
+            case "y", "Y", "н", "Н" :
+                System.out.println();
+                break;
+            case "n", "N", "т", "Т" :
+                return;
+            default: toDoOrNot();
+        }
+    }
+
+    public static void frame () {
+        System.out.println("====================");
+    }
+
+
 
 
 }
